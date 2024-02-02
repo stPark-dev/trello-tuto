0022f705c0b85e5761b557969ee33f07ef3cd2a6
stpark <dmc6618@gractor.com>
2024-02-02 10:27:06 +0900

    fix: QrCodeScanner edit

--- components/QrCodeScanner.tsx
+++ components/QrCodeScanner.tsx
@@ -4,28 +4,51 @@ import React, { useEffect, useRef } from 'react';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 
 const QrCodeScanner = ({ onScanSuccess }: { onScanSuccess: (decodedText: string) => void }) => {
-  const qrRef = useRef<HTMLDivElement>(null);
+    const qrRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (qrRef.current) {
-      const html5QrCode = new Html5QrcodeScanner(
-        qrRef.current.id,
-        { fps: 10, qrbox: 250 },
+    useEffect(() => {
+        let html5QrCodeScanner: Html5QrcodeScanner;
+
+        const onScanSuccess = (decodedText: string) => {
+            console.log(`QR Code decoded: ${decodedText}`);
+            // URL 주소인지 확인
+            if (isValidHttpUrl(decodedText)) {
+                window.location.href = decodedText; // URL로 리다이렉트
+            }
+        };
+        if (qrRef.current) {
+            html5QrCodeScanner = new Html5QrcodeScanner(
+                qrRef.current.id,
+                { fps: 10, qrbox: 250 },
         /* verbose= */ false
-      );
-      html5QrCode.render(onScanSuccess, onScanError);
-    }
+            );
+            html5QrCodeScanner.render(onScanSuccess, onScanError);
+        }
+        return () => {
+            if (html5QrCodeScanner) {
+                html5QrCodeScanner.clear().catch((err) => console.log("Error stopping QR Scanner", err));
+            }
+        };
+    }, [onScanSuccess]);
 
-    return () => {
-        // html5QrCode.clear().catch((err: any) => console.log("Error stopping QR Scanner", err));
+    // 주어진 문자열이 유효한 URL인지 확인하는 함수
+    const isValidHttpUrl = (string: string) => {
+        let url;
+
+        try {
+            url = new URL(string);
+        } catch (_) {
+            return false;
+        }
+
+        return url.protocol === "http:" || url.protocol === "https:";
     };
-  }, [onScanSuccess]);
 
-  const onScanError = (error: any) => {
-    console.log(`QR Error: ${error}`);
-  };
+    const onScanError = (error: any) => {
+        console.log(`QR Error: ${error}`);
+    };
 
-  return <div id="qr-reader" ref={qrRef} />;
+    return <div id="qr-reader" ref={qrRef} />;
 };
 
 export default QrCodeScanner;
