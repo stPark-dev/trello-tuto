c8133845d8234fbac972805adf4573cd0d7479ef
stpark <dmc6618@gractor.com>
2024-02-16 16:05:48 +0900

    feat: kakao oauth edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,30 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
           {
-            protocol: 'https',
-            hostname: 'placehold.co'
+            protocol: "https",
+            hostname: "placehold.co"
           },
           {
-            protocol: 'https',
-            hostname: 'avatars.githubusercontent.com'
+            protocol: "https",
+            hostname: "avatars.githubusercontent.com"
           },
           {
-            protocol: 'https',
+            protocol: "https",
             hostname: "lh3.googleusercontent.co"
           },
           {
-            protocol: 'https',
+            protocol: "https",
             hostname: "lh3.googleusercontent.com"
           },
           {
-            protocol: 'https',
+            protocol: "https",
             hostname: "phinf.pstatic.net"
+          },
+          {
+            protocol: "http",
+            hostname: "k.kakaocdn.net"
           }
         ]
       },
