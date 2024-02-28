0e69e57dd283ab0c2da2dbbabbb11125666e4b55
stpark <dmc6618@gractor.com>
2024-02-28 16:39:19 +0900

    feat: echarts BaseChart

--- app/(platform)/main/insights/page.tsx
+++ app/(platform)/main/insights/page.tsx
@@ -2,6 +2,7 @@
 import { Box, Button, Grid, Paper, styled } from "@mui/material"
 import { useState } from "react";
 import Component from "../_components/map";
+import EChartsBase from "@/components/echarts/BaseChart";
 
 const Item = styled(Paper)(({ theme }) => ({
     height: "100%",
@@ -19,7 +20,7 @@ const MainInsight = () => {
             <Grid container spacing={2} alignItems="flex-start" style={{ minHeight: '100vh' }}>
                 <Grid item xs={12} sm={3} container justifyContent="center" alignItems="center" spacing={2}>
                     <Grid item xs={12} >
-                        <Item sx={{ backgroundColor: "#75B5F7" }}>Item</Item>
+                        <EChartsBase theme="light" backgroundColor="#FFFFFF" dataset={[120, 200, 150, 80, 70, 110]} xAxis={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']} />
                     </Grid>
                     <Grid item xs={12}>
                         <Item sx={{ backgroundColor: "#9FEDE9" }}>Item</Item>
